6c353d04bd515dfc00dadc146d3f7194593b8e54
eshan1799 <eshan1799@gmail.com>
2020-07-15 11:59:58 +0100

    index.html

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const h1 = document.querySelector("header h1");
 h1.addEventListener("click", (event) => {
   console.log("clicked");
 });
+
+const h2 = document.querySelector("header h2");
+h2.addEventListener("mouseover", function event(){
+  h2.textContent = "Hello, Greetings"
+});
